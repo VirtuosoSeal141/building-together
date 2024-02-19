0f48e6023029b7b0ff46e192128efe0be25a4a1f
VirtuosoSeal141 <allu4kin@yandex.ru>
2024-02-20 01:33:22 +0300

    RecommendationsFix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -56,31 +56,34 @@
     </div>
     <!-- popular_catagory_area_end  -->
 
-    <!-- job_listing_area_start  -->
-    <div class="job_listing_area">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-6">
-                    <div class="section_title">
-                        <h3>Рекомендации</h3>
+    @if (count($recomendations) !== 0)
+        <!-- job_listing_area_start  -->
+        <div class="job_listing_area">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <div class="section_title">
+                            <h3>Рекомендации</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="brouse_job text-right">
+                            <a href="{{route('services-page')}}" class="boxed-btn4">Показать ещё</a>
+                        </div>
                     </div>
                 </div>
-                <div class="col-lg-6">
-                    <div class="brouse_job text-right">
-                        <a href="{{route('services-page')}}" class="boxed-btn4">Показать ещё</a>
+                <div class="job_lists">
+                    <div class="row">
+                        @foreach ($recomendations as $recomendation)
+                            <x-service :service="$recomendation"></x-service>
+                        @endforeach
                     </div>
-                </div>
-            </div>
-            <div class="job_lists">
-                <div class="row">
-                    @foreach ($recomendations as $recomendation)
-                        <x-service :service="$recomendation"></x-service>
-                    @endforeach
                 </div>
             </div>
         </div>
-    </div>
-    <!-- job_listing_area_end  -->
+        <!-- job_listing_area_end  -->
+    @endif
+
     @if (!Auth::user())
         <!-- job_searcing_wrap  -->
         <div class="job_searcing_wrap overlay">
